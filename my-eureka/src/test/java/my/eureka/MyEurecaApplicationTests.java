92e09125ceb7e7f6bfae705924e881b2a79077aa
olga <arapovaolga1999@gmail.ru>
2023-04-04 22:06:45 +0500

    Eureca server created

---
+++ my-eureka/src/test/java/my/eureka/MyEurecaApplicationTests.java
@@ -0,0 +1,13 @@
+package my.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEurecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
