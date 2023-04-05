6616835bf7b6d611e103258ea4964c5ae24774db
olga <arapovaolga1999@gmail.ru>
2023-04-05 18:32:56 +0500

    services created

---
+++ punishments/src/test/java/my/punishments/PunishmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.punishments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunishmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
